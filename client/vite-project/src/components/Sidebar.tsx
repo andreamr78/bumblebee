7663a7e1496f16604910d819e38fab9df3257fc8
Andrea <Omar@Andreas-MacBook-Air.local>
2025-01-16 21:51:20 -0600

    update dashbard and unfinished styles

---
+++ client/vite-project/src/components/Sidebar.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/Logo.svg'
+import '../styles/ComponentsStyles.css'
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <img src={logo} alt="" />
+        <h1>Cinematch</h1>
+        <div className='sidebar-links'>
+            <p><Link to="/">All</Link></p>
+            <p><Link to="/">Watched</Link></p>
+            <p><Link to="/">Log Out</Link></p>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ client/vite-project/src/components/Topbar.tsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+import { Navbar, Container, Button, Offcanvas } from "react-bootstrap"
+import logo from '../assets/Logo.svg'
+import { Link } from "react-router-dom"
+import '../styles/ComponentsStyles.css'
+
+function Topbar(props:any) {
+  return (
+    <div>
+        <Container className="topbar-full-view">
+            <Navbar className="bg-body-tertiary justify-content-between">
+                <Navbar.Brand href="/">Welcome {props.name}</Navbar.Brand>
+                <Button href="">Start swiping!</Button>
+            </Navbar>
+      </Container> 
+      
+    <Container className="topbar-mobile-view">
+        <Navbar key='sm' expand='sm'  className="bg-body-tertiary justify-content-between">
+          <Navbar.Brand href="/">
+            <img src={logo} alt="" />
+            Welcome {props.name}
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='offcanvasNavbar-expand-sm'/>
+            <Navbar.Offcanvas placement="end">
+              <Offcanvas.Header closeButton></Offcanvas.Header>
+              <Offcanvas.Body>
+                <Link to="/">All</Link>
+                <Link to="/">Watched</Link>
+                <Link to="/">Log Out</Link>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+            </Navbar>
+        </Container>
+
+    </div>
+  )
+}
+
+export default Topbar
