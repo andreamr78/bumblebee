db8faefdfd7e48a9d9794ac37fb536db971dae09
mikealiaga <mikealiaga97@gmail.com>
2025-01-20 20:28:51 -0600

    Styled swipe interface and Card

--- client/vite-project/src/views/Swipe.tsx
+++ client/vite-project/src/views/Swipe.tsx
@@ -1,13 +1,26 @@
 // import React from 'react';
 import MovieCard from '../components/MovieCard';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useNavigate} from "react-router-dom";
+
 
 function Swipe() {
+
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
-      <div>
+      <div className="d-flex p-4 back" onClick={handleBackClick} style={{ cursor: 'pointer' }}>
+        <i className="bi bi-arrow-left pe-3"></i>        
         <h5>Back</h5>
-      </div>    
-      <MovieCard />
+      </div>
+      <div className="d-flex justify-content-center">    
+        <MovieCard />
+      </div>
     </div>
   );
 }
